9afb66c9be5bf3546bb5cb9b0a1e01d716106fdd
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-04-08 16:56:40 +0300

    Fonts removed

--- art_project/accounts/tests/views/test_register_view.py
+++ art_project/accounts/tests/views/test_register_view.py
@@ -9,65 +9,7 @@ from art_project.art_portal_app.models import Painting, Style, Technique, Galler
 UserModel = get_user_model()
 
 
-class RegisterViewTests(TestCase, ArrangeMixin):
-    # VALID_USER_CREDENTIALS = {
-    #     'username': 'testuser',
-    #     'password': 'asd123',
-    #     'first_name': 'Test',
-    #     'last_name': 'User',
-    #     'email': 'test.user@mail.com'
-    # }
-    #
-    # OTHER_USER_CREDENTIALS = {
-    #     'username': 'otheruser',
-    #     'password': 'asd123',
-    #     'first_name': 'Other',
-    #     'last_name': 'User',
-    #     'email': 'other.user@mail.com'
-    # }
-    #
-    # VALID_PAINTING_DATA = {
-    #     'title': 'Mona Lisa',
-    #     'width': 100,
-    #     'height': 100,
-    #     'base_material': Painting.CANVAS,
-    #     'photo': 'http://monalisa/url.png',
-    #     'price': 1000,
-    #     'main_colors': 'blue'
-    # }
-    #
-    # def __create_valid_user_and_profile(self):
-    #     # create user
-    #     user = UserModel.objects.create_user(**self.VALID_USER_CREDENTIALS)
-    #     # get the profile which is created automatically
-    #     profile = Profile.objects.get(pk=user.pk)
-    #     return user, profile
-    #
-    # def __create_other_user(self):
-    #     user = UserModel.objects.create_user(**self.OTHER_USER_CREDENTIALS)
-    #     # get the profile which is created automatically
-    #     profile = Profile.objects.get(pk=user.pk)
-    #     return user, profile
-    #
-    # def __create_style(self):
-    #     style = Style.objects.create(style_name='Impressionism')
-    #     return style
-    #
-    # def __create_technique(self):
-    #     technique = Technique.objects.create(technique_name='Oil')
-    #     return technique
-    #
-    # def __create_gallery(self):
-    #     gallery = Gallery.objects.create(name='Maestro', address='Sofia')
-    #     return gallery
-    #
-    # def __create_painting(self, user):
-    #     style = self.__create_style()
-    #     technique = self.__create_technique()
-    #     gallery = self.__create_gallery()
-    #     painting = Painting.objects.create(
-    #         **self.VALID_PAINTING_DATA, style=style, techniques=technique, gallery=gallery, artist=user)
-    #     return painting
+class RegisterViewTests(ArrangeMixin, TestCase):
 
     def test__expect_correct_template_used(self):
 
